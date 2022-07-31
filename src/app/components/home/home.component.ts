d1d67bb6aa2954ee7ab2017b7ce8c1cabeabec8a
Danish Javed <danishjaved636@gmail.com>
2022-07-31 21:18:33 +0530

    Page optimized for now

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,76 +1,63 @@
-import { HtmlParser } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
+  
 export class HomeComponent implements OnInit {
   sectionIndex: number = 1;
   containers = document.getElementsByClassName('home-element-container');
-  sections: [HTMLElement | null] = [null];
-  constructor(private router: Router) {}
+  sections: [Element | null] = [this.containers[0]];
 
-  showDetail(aboutSelected: string) {
-    document
-      .querySelectorAll('.about-pannel')
-      .forEach((ele) => ele.classList.remove('active'));
-    document
-      .querySelectorAll('.detail')
-      .forEach((ele) => ele.classList.add('hide'));
-    document.getElementById(aboutSelected)?.classList.add('active');
-    document
-      .getElementById(aboutSelected + '-details')
-      ?.classList.replace('hide', 'show');
-    document
-      .getElementById(aboutSelected + '-icon')
-      ?.classList.replace('hide', 'show');
-  }
+  constructor() { }
 
-  showWorkDetail(workSelected: string) {
-    console.log(workSelected);
-    document
-      .querySelectorAll('.work-pannel')
-      .forEach((ele) => ele.classList.remove('active'));
-    document
-      .querySelectorAll('.my-work .work-detail')
-      .forEach((ele) => ele.classList.add('hide'));
-    ;
-    document.getElementById(workSelected)?.classList.add('active');
+  showDetail(selectedPannelId: string, sectionDetailClass: string, activeClass: string) {
 
-    document
-      .getElementById(workSelected + '-detail')
-      ?.classList.replace('hide', 'show');
-    // document
-    //   .getElementById(workSelected + '-icon')
-    //   ?.classList.replace('hide', 'show');
+    // hiding all the visbile details 
+    document.querySelectorAll('.' + sectionDetailClass).forEach((ele) => {      
+      ele.classList.add('hide');
+    });
+
+    //removing active class from previous selection
+    let selectedPaneSibling = document.querySelector('#' + selectedPannelId)?.parentElement?.children;
+
+    if (selectedPaneSibling)
+      for (let element of selectedPaneSibling)
+        element.classList.remove(activeClass);
+    
+    //adding active class to selected pane
+    document.getElementById(selectedPannelId)?.classList.add(activeClass);
+
+    //making details and icons visible
+    document.querySelector('.' + sectionDetailClass + '#' + selectedPannelId + 'Detail')?.classList.replace('hide', 'show');
+    document.querySelector('.' + sectionDetailClass + '#' + selectedPannelId + 'Icon')?.classList.replace('hide', 'show');
+
   }
 
   scroll(id: string | undefined) {
-    let scrollButton = document.getElementById('scroll-button');
+    let scrollButtonIcon = document.getElementById('scrollButtonIcon');
 
     this.sectionIndex++;
 
     let element = this.sections[this.sectionIndex];
 
+    //to handle clicks from navbar
     if (id != undefined) {
       element = document.getElementById(id);
       this.sectionIndex = this.sections.indexOf(element);
-      console.log(this.sections.length, this.sectionIndex);
     }
 
+    // In case its not the last section, arrow must point down
     if (this.sectionIndex < this.sections.length - 1) {
-      scrollButton?.classList.replace('fa-arrow-up', 'fa-arrow-down');
+      scrollButtonIcon?.classList.replace('fa-arrow-up', 'fa-arrow-down');
     }
 
-    // if (this.sectionIndex == this.sections.length-1)
-    // { scrollButton?.classList.remove('fa-arrow-down'); scrollButton?.classList.add('fa-arrow-up'); console.log(scrollButton?.classList); }
-
+    // In case its last section, arrow must point up and
     if (this.sectionIndex == this.sections.length - 1) {
       this.sectionIndex = 0;
-      scrollButton?.classList.replace('fa-arrow-down', 'fa-arrow-up');
+      scrollButtonIcon?.classList.replace('fa-arrow-down', 'fa-arrow-up');
     }
 
     element?.scrollIntoView({
@@ -81,15 +68,18 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    document.getElementById('scrollButton')?.focus();
     setTimeout(() => {
       document
-        .getElementById('skill-details')
+        .getElementById('aboutSkillDetail')
         ?.classList.replace('hide', 'show');
-      document.getElementById('skill-icon')?.classList.replace('hide', 'show');
+      document
+        .getElementById('aboutSkillIcon')
+        ?.classList.replace('hide', 'show');
     }, 500);
 
-    //to get the parent element of all containers
+    // //to get the parent element of all containers
     for (let i = 0; i < this.containers.length; i++)
-      this.sections.push(this.containers[i].parentElement);
+      this.sections.push(this.containers[i]);
   }
 }
